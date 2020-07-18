be75d331b46af67a8db8ba98c81aea50ae2c9937
mgarcife42 <32391510+mgarcife42@users.noreply.github.com>
2020-07-18 02:43:00 +0200

    Update fillvector2.m

--- fillvector2.m
+++ fillvector2.m
@@ -1,3 +1,5 @@
+% Crea un vector de longitud N de la forma (x1,x1+D,x1+2D,...x1+(N-1)*D)
+% Ejemplo: fillvector2(1,1,10) genera (1,2,3,4,5,6,7,8,9,10)
 function x = fillvector2(x1,D,N)
   for n=1:N
     x(n)=x1+D*(n-1);
